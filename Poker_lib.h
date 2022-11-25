f874439c4b7a0ff5b216f5832c0480f341bc6e8e
Irteza Faisal <26100163@lums.edu.pk>
2022-11-25 10:20:10 +0500

    Doing stuff

--- Poker_lib.h
+++ Poker_lib.h
@@ -2,12 +2,12 @@ using namespace std;
 
 class POKER {
     public:
-    int Nums = [1,2,3,4,5,6,7,8,9,10,11,12,13]; /*Nums and Suits define the Number of cards per suit (13) A,1,2,3,4,5,6,7,8,9,10,J,Q,K*/
-    char suits = ['D','H','C','S'];
+    int Nums[13] = {1,2,3,4,5,6,7,8,9,10,11,12,13}; /*Nums and Suits define the Number of cards per suit (13) A,1,2,3,4,5,6,7,8,9,10,J,Q,K*/
+    char suits[4] = {'D','H','C','S'};
+    int game_state = 0;
+    /*int Initial_game_states[3] = {1,2,-1}; Defines initial game states of Start game, Rules, Exit game, 1 starts game, 2 is rules, -1 Exit game*/
 
-    int Initial_game_states = [1,2,-1]; /*Defines initial game states of Start game, Rules, Exit game, 1 starts game, 2 is rules, -1 Exit game*/
-
-    int Start_sub_states = [1,2,3] /*Substates when in Start game to choose between number of players 1->3 2->4 3->5*/
+    /*int Start_sub_states[3] = {1,2,3}; Substates when in Start game to choose between number of players 1->3 2->4 3->5*/
 
     typedef struct {
         int Number;
@@ -16,11 +16,11 @@ class POKER {
 
     card cards[52];
 
-    void init_deck(){
+    void init_deck(){ /*Initialises deck*/
         int n = 0;
         while (n != 52){
         for (int i = 0; i < 4; i++){
-            for (int j = 0; j < 13){
+            for (int j = 0; j < 13; j++){
                 cards[n].Number = Nums[j];
                 cards[n].suit = suits[i];
                 n++;
@@ -28,6 +28,21 @@ class POKER {
         }
         }
     }
-    
+
+    void INIT(){
+        cout<<setw(125)<<"<----WELCOME TO POKER TEXAS HOLD'EM---->\n";
+        cout<<"1:Start Game.\n2:Rules.\n-1:Exit.\n";
+        do{
+            cout<<"-->";
+            cin>>game_state;
+        }while(game_state != 1 || game_state != 2 || game_state != -1 );
+        
+        POKER::change_gamestate_mainmenu();
+
+    }
+
+    void change_gamestate_mainmenu(){
+
+    }
 };
 
--- Texas_Hold_em.cpp
+++ Texas_Hold_em.cpp
@@ -6,15 +6,7 @@ using namespace std;
 
 /*<---Main Function--->*/
 int main(){
-    int game = 0;
-    while (game != -1 ){
-        cout<<setw(135)<<"------------>Welcome to Texas Hold'em<------------\n";
-        cout<<"Enter :\n"
-            <<"\t 1:Start Bot match (1P vs 3B)\n"
-            <<"\t 2:Rules\n"
-            <<"\t-1:Exit\n";
-        cout<<"-->";
-        cin>>game;   
-    }
+    POKER game;
+    game.INIT();
     return 0;
 }
